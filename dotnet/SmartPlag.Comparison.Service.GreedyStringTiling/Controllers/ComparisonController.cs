fdf5b01448fa0dc8a0f6a027555307ff804ac0b0
Markus Dietrich <mdi1984@gmail.com>
2016-06-02 20:36:42 +0200

    added comparison controller

---
+++ dotnet/SmartPlag.Comparison.Service.GreedyStringTiling/Controllers/ComparisonController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SmartPlag.Comparison.Service.GreedyStringTiling.Model;
+namespace SmartPlag.Comparison.Service.GreedyStringTiling.Controllers
+{
+  [Route("api/[controller]")]
+  public class ComparisonController
+  {
+    [HttpPost]
+    public IActionResult SimpleComparison([FromBody] ComparisonRequest request)
+    {
+      try
+      {
+        return new ObjectResult(null);
+      }
+      catch (Exception)
+      {
+        return new BadRequestObjectResult("Invalid request format");
+      }
+    }
+  }
+}
