f55f8358d7ae1407f7c4701c49379fd8752830ba
Markus Dietrich <mdi1984@gmail.com>
2016-06-11 21:39:50 +0200

    added efcore with tools

--- dotnet/SmartPlag.Manager.SimpleManager/Startup.cs
+++ dotnet/SmartPlag.Manager.SimpleManager/Startup.cs
@@ -9,21 +9,18 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace SmartPlag.Manager.SimpleManager
 {
-    public class Startup
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
     }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("Hello World!");
+      });
+    }
+  }
 }
