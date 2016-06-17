87b7d1ad34c4dd3ffea7a776360632c59cb0fe34
Markus Dietrich <mdi1984@gmail.com>
2016-06-17 12:14:40 +0200

    initial assignments controller and view

---
+++ dotnet/SmartPlag.Manager.SimpleManager/Controllers/AssignmentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SmartPlag.Manager.Simple.EF;
+
+namespace SmartPlag.Manager.SimpleManager.Controllers
+{
+  public class AssignmentsController : Controller
+  {
+    private AssignmentManager manager;
+
+    public AssignmentsController(AssignmentManager manager)
+    {
+      this.manager = manager;
+    }
+
+    [Authorize]
+    public IActionResult Index()
+    {
+
+      return View();
+    }
+  }
+}
